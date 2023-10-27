1f07a6c9f8627b7bd6a75c5bbed2bfe5b139f06b
Nguyễn Hữu Duy Kha <nhokkha345@gmail.com>
2023-10-27 13:08:27 +0700

    add col of archive in home page

--- wp-content/themes/twentytwentyone/header.php
+++ wp-content/themes/twentytwentyone/header.php
@@ -17,21 +17,45 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/f6dce9b617.js" crossorigin="anonymous"></script>
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content">
-		<?php
-		/* translators: Hidden accessibility text. */
-		esc_html_e( 'Skip to content', 'twentytwentyone' );
-		?>
-	</a>
+<div id="page" class="site ">
+    <a class="skip-link screen-reader-text" href="#content">
+        <?php
+        /* translators: Hidden accessibility text. */
+        esc_html_e('Skip to content', 'twentytwentyone');
+        ?>
+    </a>
 
-	<?php get_template_part( 'template-parts/header/site-header' ); ?>
-
-	<div id="content" class="site-content">
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main">
+    <?php get_template_part('template-parts/header/site-header'); ?>
+    <div class="main"  style="position: relative;">
+        <div class="col-md-3 archive"style="position: absolute;left: 0;display: flex;
+        justify-content: end;">
+            <div class="vc_column-inner">
+                <div class="wpb_wrapper">
+                    <div class="wpb_text_column wpb_content_element">
+                        <div class="wpb_wrapper">
+                            <?php
+                            if (is_home() || is_front_page()) { // Kiểm tra xem bạn đang ở trang chủ
+                                // Sử dụng hàm get_archives để lấy danh sách các bài viết theo thời gian
+                                $args = array(
+                                    'type' => 'monthly', // Loại archive (có thể là 'monthly', 'yearly', 'daily', và nhiều loại khác)
+                                );
+                                $archives = wp_get_archives( $args );
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
